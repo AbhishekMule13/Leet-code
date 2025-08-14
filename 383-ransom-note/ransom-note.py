ded25cb0363f10728a22566e759d6680b179478e
Abhishek Mule <abhimule1308@gmail.com>
2025-08-14 11:58:00 +0530

    Time: 7 ms (95.82%) | Memory: 12.6 MB (78.20%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -5,10 +5,15 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
+        '''
         for ch in ransomNote:
             if ch in magazine:
                 magazine=magazine.replace(ch,"",1)
             else :
                 return False
         return True            
-        
+        '''
+        for i in set(ransomNote):
+            if(magazine.count(i)< ransomNote.count(i)):
+                return False
+        return True        
