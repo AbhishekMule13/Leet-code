031555d682680ad89485a9e2f77575a4a8d8a7e8
Abhishek Mule <abhimule1308@gmail.com>
2025-09-18 11:50:16 +0530

    Time: 3 ms (56.16%) | Memory: 12.7 MB (35.98%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.py
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -5,7 +5,8 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        res = ""
-        for i in range(0, len(s), 2 * k):
-            res += s[i:i+k][::-1] + s[i+k:i+2*k]
-        return res
+        s=list(s)
+        for i in range (0, len(s), 2*k):
+            s[i:i+k]=reversed (s[i:i+k])
+        return "".join(s)    
+        
