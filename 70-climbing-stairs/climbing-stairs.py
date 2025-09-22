5e56baa313d26b2bcc9ff189776106f9537f29d7
Abhishek Mule <abhimule1308@gmail.com>
2025-09-22 21:40:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (54.23%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -7,8 +7,8 @@ class Solution(object):
         if n <= 2:
             return n
         
-        one, two = 1, 2  # f(1), f(2)
+        f1, f2 = 1, 2  
         for i in range(3, n + 1):
-            one, two = two, one + two  # shift forward
-        return two
+            f1, f2 = f2, f1 + f2  
+        return f2
         
